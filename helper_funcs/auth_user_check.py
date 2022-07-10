4a908381ae1b4c888976942940715e9c3143c399
HuzunluArtemis <84624971+HuzunluArtemis@users.noreply.github.com>
2022-07-10 15:36:38 +0300

    upgrade

--- helper_funcs/auth_user_check.py
+++ helper_funcs/auth_user_check.py
@@ -4,6 +4,6 @@ from config import Config
 
 def AuthUserCheck(message):
     if 0 in Config.AUTH_IDS: return True
-    elif message.from_user.id in Config.AUTH_IDS or Config.OWNER_ID: return True
+    elif message.from_user.id in (Config.AUTH_IDS or Config.OWNER_ID): return True
     elif message.chat.id in Config.AUTH_IDS: return True
     else: return False
